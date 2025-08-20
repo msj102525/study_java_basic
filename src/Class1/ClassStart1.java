d8fe9ddac0ea536395887cb1e2bf4045933920b4
DoorWinBell <msj102525@gmail.com>
2025-08-20 19:13:34 +0900

    DoorWinBell-20250820

---
+++ src/Class1/ClassStart1.java
@@ -0,0 +1,15 @@
+package Class1;
+
+public class ClassStart1 {
+    public static void main(String[] args) {
+
+        String student1Name = "학생1";
+        int student1Age = 15;
+        int student1Grade = 90;
+
+        String student2Name = "학생2";
+        int student2Age = 15;
+        int student2Grade = 90;
+
+    }
+}
